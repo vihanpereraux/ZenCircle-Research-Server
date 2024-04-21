669542a22fcb0352de14db475947e55ba23a56f0
Vihan Perera <vihanpereraux@gmail.com>
2024-04-21 06:34:44 +0100

    implement basic model communication

--- Controller/universalController.py
+++ Controller/universalController.py
@@ -1,8 +1,9 @@
+from openai import OpenAI
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
 
 # GET
 @app.route("/", methods=['GET'])
@@ -12,12 +13,27 @@ def getFunction():
 # POST
 @app.route("/get-response", methods=['POST'])
 def getResponse():
-  # if request.json or 'name' not in request.json:
-  #   return jsonify({'error': 'Ivalid Payload !!'}), 400
+  content = request.args.get('content')
+  
+  system_content = "Always respond in a very human-like behaviour"
+  
+  # Point to the local server
+  client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+  completion = client.chat.completions.create(
+    model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
+    messages=[
+      {"role": "system", "content": system_content},
+      {"role": "user", "content": content}
+    ],
+    temperature=0.7,
+  )
+
+  # print(completion.choices[0].message)
   
   item = {
     'item_id' : 1,
-    'message' : 'Im doing good !! '    
+    'message' : completion.choices[0].message.content   
   }
   
   return jsonify(item), 201
@@ -25,27 +41,4 @@ def getResponse():
 if __name__ == "__main__":
   app.run(debug=True) 
   
-  
-  
-  
 
-# Example: reuse your existing OpenAI setup
-from openai import OpenAI
-
-# Point to the local serverls
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-
-completion = client.chat.completions.create(
-  model="YanaS/llama-2-7b-langchain-chat-GGUF",
-  messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
-  ],
-  temperature=0.7,
-)
-
-print(completion.choices[0].message.content)
-
-
-# def mainFunc():
-#     print("Universal Copntroller is running")
