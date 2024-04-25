c801a8325fc22d70a550e159aa113874808fa2cd
Vihan Perera <vihanpereraux@gmail.com>
2024-04-25 12:31:58 +0100

    check prompt templtes for m7b

--- Model/communication_model.py
+++ Model/communication_model.py
@@ -38,22 +38,28 @@ def manage_user_data(action, username, history):
     
 # <s>[/INST] I feel so overwhelmed today</s>[INST] Model [/INST] Behave as a mental health professional and generate answers on a human-like approach. Use kind and lovely words all the time when ending a sentence<s>[INST]
 def manage_conversation(content, username, history):
-    system_content = "Always respond in a very human-like behaviour and here's the conversation history - " + str(history) + "It has the user input and the AI generated answer in a json format wrapped in an array. When generaring something always refer to the conversation history. Don't generate answers that has more that 40 words."
-  
+    system_content = "Always respond in a very human-like behaviour and here's the conversation history - " + str(history) + "When generating an answer always refer to the conversation history. Strictly Don't generate answers that has more that 30 words."
+    system_content_2 = "Always repond as a kind, loving lady while referencing to the conversation history -" + str(history) + "Strictly Don't generate answers that has more that 50 words"
+    system_content_3 = "Always repond as a kind, loving lady"
+    
     #point to the local server
     client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
     # prompt reshaping - v.0
     updated_content = "<s>[/INST]" + str(content) + "</s>[INST] Model [/INST] Behave as a mental health professional and generate answers on a human-like approach. Use kind and lovely words all the time when ending a sentence<s>[INST]"
+    updated_content_2 = "<s>[INST] "+ str(content) + " [/INST] Sad mood right now"
+    updated_content_3 = "[INST] "+ str(content) + " [/INST]" 
+    
 
     completion = client.chat.completions.create(
-        model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
-        messages=[
-            {"role": "system", "content": system_content},
-            {"role": "user", "content": updated_content}
-        ],
-        temperature=0.81,
+    model="TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
+    messages=[
+        {"role": "system", "content": system_content_2},
+        {"role": "user", "content": updated_content_2}
+    ],
+    temperature=0.7,
     )
+    
     response = completion.choices[0].message.content
     # updates the permenant conversation history
     manage_user_data('update_document', username, history)
